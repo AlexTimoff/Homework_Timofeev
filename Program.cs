4c0df47474c9f8f5083f467083a1f8fcc8dd681d
AlexTimoff <alekstimofeev007@mail.ru>
2024-01-28 12:25:17 +0500

    Добавлены решения

--- Program.cs
+++ Program.cs
@@ -5,71 +5,71 @@
 // в промежутке от M до N. Использовать рекурсию, не
 // использовать циклы.
 
-System.Console.WriteLine("Введите число M");
-int M = Convert.ToInt32(Console.ReadLine());;
-System.Console.WriteLine("Введите число N");
-int N = Convert.ToInt32(Console.ReadLine());;
+// System.Console.WriteLine("Введите число M");
+// int M = Convert.ToInt32(Console.ReadLine());;
+// System.Console.WriteLine("Введите число N");
+// int N = Convert.ToInt32(Console.ReadLine());;
 
-if (M==N)
-{
-    Console.WriteLine("Вы ввели одно и то же число: " + M+". Попробуйте снова.");
-}
+// if (M==N)
+// {
+//     Console.WriteLine("Вы ввели одно и то же число: " + M+". Попробуйте снова.");
+// }
 
-else
-{
-int minNum=Int32.Min(M,N);
-int maxNum=Int32.Max(M,N);
-PrintNumbers(minNum, maxNum);
-}
+// else
+// {
+// int minNum=Int32.Min(M,N);
+// int maxNum=Int32.Max(M,N);
+// PrintNumbers(minNum, maxNum);
+// }
 
-void PrintNumbers(int firstNum, int lastNum)
-{
-    if(firstNum>lastNum)
-    {
-        return;
-    }
-    System.Console.Write(firstNum+"\t");
-    PrintNumbers(firstNum+1, lastNum);
-}
+// void PrintNumbers(int firstNum, int lastNum)
+// {
+//     if(firstNum>lastNum)
+//     {
+//         return;
+//     }
+//     System.Console.Write(firstNum+"\t");
+//     PrintNumbers(firstNum+1, lastNum);
+// }
 
 
-// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-System.Console.WriteLine("Введите неотрицательное число m");
-int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите неотрицательное число m");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine("Введите неотрицательное число n");
-int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите неотрицательное число n");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int AckermannFunction(int x, int y)
-{
-    if (x == 0)
-    {
-        return y + 1;
-    }
-    else if (x > 0 && y == 0)
-    {
-        return AckermannFunction(x - 1, 1);
-    }
-    else
-    {
-        return AckermannFunction(x - 1, AckermannFunction(x, y - 1));
-    }
-}
+// int AckermannFunction(int x, int y)
+// {
+//     if (x == 0)
+//     {
+//         return y + 1;
+//     }
+//     else if (x > 0 && y == 0)
+//     {
+//         return AckermannFunction(x - 1, 1);
+//     }
+//     else
+//     {
+//         return AckermannFunction(x - 1, AckermannFunction(x, y - 1));
+//     }
+// }
 
-Console.WriteLine($"A({m},{n}) = {AckermannFunction(m,n)}");
+// Console.WriteLine($"A({m},{n}) = {AckermannFunction(m,n)}");
 
-// Задача 3: Задайте произвольный массив. Выведете
-// его элементы, начиная с конца. Использовать
-// рекурсию, не использовать циклы.
+// // Задача 3: Задайте произвольный массив. Выведете
+// // его элементы, начиная с конца. Использовать
+// // рекурсию, не использовать циклы.
 
 
 int[] matrix = CreateArray();
-GetArray(matrix);
+GetArray(matrix); //стандартным образом задаем массив
 PrintArray(matrix); //выводим созданный массив для наглядности
 System.Console.WriteLine();
 int index = 0;
-PrintReverseArray(matrix, index); //выводим массив в обратном порядке
+PrintReverseArray(matrix, index); //С ИСПОЛЬЗОВАНИЕМ РЕКУРСИИ ВЫВОДИМ МАССИВ В ОБРАТНОМ ПОРЯДКЕ
 
 
 int[] CreateArray()
